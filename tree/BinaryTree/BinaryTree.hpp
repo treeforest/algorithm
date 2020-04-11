543debc279f767d6e2031f0e121bb9e512ee81a0
Figzy <1451670604@qq.com>
2020-04-12 01:29:01 +0800

    feat: init BinaryTree

---
+++ tree/BinaryTree/BinaryTree.hpp
@@ -0,0 +1,545 @@
+#pragma once
+#include <stdlib.h>
+#include <stack>
+#include <queue>
+using namespace std;
+
+template<typename ElemType>
+struct BTNode
+{
+	ElemType val;
+	struct BTNode<ElemType>* lchild;
+	struct BTNode<ElemType>* rchild;
+	struct BTNode<ElemType>* parent;
+};
+
+/*
+ * 二叉树(二叉搜索树)
+ *
+ * 1、ElemType 二叉树存储的数据类型
+ * 2、Compare: 元素 ElemType 的比较函数，
+ *			函数原型为：size_t (*Compare)(ElemType v, ElemType w)
+ *			返回值类型：
+ *					(1)当 v > w 时返回 >0
+ *					(2)当 v = w 时返回 =0
+ *					(3)当 v < w 时返回 <0
+ */
+template<typename ElemType, typename Compare>
+class BTree
+{
+public:
+	BTree(Compare cmp);
+	~BTree();
+
+	bool Insert(const ElemType& _Val);
+	bool Delete(const ElemType& _Val);
+
+	void PreOrder(void (*visit)(const ElemType& _Val));
+	void InOrder(void (*visit)(const ElemType& _Val));
+	void PostOrder(void (*visit)(const ElemType& _Val));
+	void LevelOrder(void (*visit)(const ElemType& _Val));
+
+	ElemType Minimum() const;
+	ElemType Maximum() const;
+
+	size_t Size() const;
+	size_t Depth() const;
+	size_t Empty() const;
+
+private:
+	void destroy(BTNode<ElemType>* T);
+	BTNode<ElemType>* search(const ElemType& _Val);
+	BTNode<ElemType>* minimum(BTNode<ElemType>* T);
+	BTNode<ElemType>* maximun(BTNode<ElemType>* T);
+
+private:
+	BTNode<ElemType>* m_root;
+	size_t m_size;
+	Compare m_cmp;
+};
+
+/*
+ * 构造函数
+ *
+ * 参数
+ *  cmp ：为元素 ElemType 的比较函数的指针
+ *        函数原型为：size_t (*Compare)(ElemType v, ElemType w)
+ *	      cmp 返回值要求：
+ *					(1)当 v > w 时返回 >0
+ *					(2)当 v = w 时返回 =0
+ *					(3)当 v < w 时返回 <0
+ */
+template<typename ElemType, typename Compare>
+inline BTree<ElemType, Compare>::BTree(Compare cmp)
+{
+	m_root = NULL;
+	m_size = 0;
+	m_cmp = cmp;
+}
+
+/*
+ * 析构函数
+ */
+template<typename ElemType, typename Compare>
+BTree<ElemType, Compare>::~BTree()
+{
+	Destroy(m_root);
+}
+
+/*
+ * 在二叉树中插入一个元素
+ *
+ * 参数
+ *  _Val ：插入的元素值
+ *
+ * 返回值
+ *  bool ：如果插入成功(即树中不存在该元素)，则返回 true
+ *         如果插入失败，返回 false
+ */
+template<typename ElemType, typename Compare>
+bool BTree<ElemType, Compare>::Insert(const ElemType& _Val)
+{
+	BTNode<ElemType>* p = NULL;
+	BTNode<ElemType>* q = NULL;
+	BTNode<ElemType>* node = NULL;
+
+	// Find node's parent
+	// Left child is less to parent
+	// Right child is greater than or equal to parent
+	p = m_root;
+	while (p) {
+		q = p;
+		if (m_cmp(p->val, _Val) < 0) {
+			p = p->lchild;
+		}
+		else {
+			p = p->rchild;
+		}
+	}
+
+	// Insert the node into the leaf node
+	node = new BTNode<ElemType>();
+	node->lchild = NULL;
+	node->rchild = NULL;
+	node->val = _Val;
+	node->parent = q;
+
+	if (q == NULL) {
+		m_root = p; // tree was empty
+	}
+	else if (node->val < q->val) {
+		q->lchild = node;
+	}
+	else {
+		q->rchild = node;
+	}
+
+	++m_size;
+}
+
+/*
+ * 删除二叉树中的元素
+ *
+ * 参数
+ *  _Val ：删除的元素值
+ *
+ * 返回值
+ *  bool ：如果删除成功(当存在该值为_Val的节点)，返回 true
+ *         如果删除失败，返回 false
+ */
+template<typename ElemType, typename Compare>
+bool BTree<ElemType, Compare>::Delete(const ElemType& _Val)
+{
+	BTNode<ElemType>* p = NULL;
+	BTNode<ElemType>* q = NULL;
+
+	// Search the node 
+	p = Search(_Val);
+	if (!p) {
+		return false;
+	}
+
+	if (p == m_root) {
+		// no left child
+		if (!p->lchild) {
+			if (p->rchild) {
+				p->rchild->parent = NULL;
+			}
+			m_root = p->rchild;
+		}
+		// only left child
+		else if (p->lchild && !p->rchild) {
+			p->lchild->parent = NULL;
+			m_root = p->lchild;
+		}
+		// two child
+		else {
+			if (!p->rchild->lchild) {
+				p->rchild->lchild = p->lchild;
+				p->rchild->parent = NULL;
+				p->lchild->parent = p->rchild;
+				m_root = p->rchild;
+			}
+			else {
+				q = Minimum(p->rchild);
+
+				q->parent->lchild = q->rchild;
+				if (q->rchild) {
+					q->rchild->parent = q->parent;
+				}
+
+				q->lchild = p->lchild;
+				q->rchild = p->rchild;
+				q->parent = NULL;
+				p->lchild->parent = q;
+				p->rchild->parent = q;
+				m_root = q;
+			}
+		}
+	}
+	else {
+		// no left child
+		if (!p->lchild) {
+			if (p == p->parent->lchild) {
+				p->parent->lchild = p->rchild;
+			}
+			else {
+				p->parent->rchild = p->rchild;
+			}
+
+			if (p->rchild) {
+				p->rchild->parent = p->parent;
+			}
+		}
+		// only left child
+		else if (p->lchild && !p->rchild) {
+			if (p == p->parent->lchild) {
+				p->parent->lchild = p->lchild;
+			}
+			else {
+				p->parent->rchild = p->lchild;
+			}
+
+			p->lchild->parent = p->parent;
+		}
+		// two child
+		else {
+			if (!p->rchild->lchild) {
+				p->lchild->parent = p->rchild;
+				p->rchild->lchild = p->lchild;
+				p->rchild->parent = p->parent;
+			}
+			else {
+				q = Minimum(p->rchild);
+
+				q->parent->lchild = q->rchild;
+				if (q->rchild) {
+					q->rchild->parent = q->parent;
+				}
+
+				q->lchild = p->lchild;
+				q->rchild = p->rchild;
+				q->parent = p->parent;
+				if (p == p->parent->lchild) {
+					p->parent->lchild = q;
+				}
+				else {
+					p->parent->rchild = q;
+				}
+				p->lchild->parent = q;
+				p->rchild->parent = q;
+			}
+		}
+	}
+
+	delete p;
+	p = NULL;
+	--m_size;
+
+	return true;
+}
+
+/*
+ * 二叉树前序遍历
+ *
+ * 参数
+ *  visit ：该参数为访问树节点的函数指针，以实现对树节点的相关操作
+ */
+template<typename ElemType, typename Compare>
+inline void BTree<ElemType, Compare>::PreOrder(void(*visit)(const ElemType& _Val))
+{
+	stack<BTNode<ElemType>*> st;
+	BTNode<ElemType>* p = m_root;
+
+	while (p || !st.empty()) {
+		if (p) {
+			visit(p->val);
+			if (p->rchild) {
+				st.push(p->rchild);
+			}
+			p = p->lchild;
+		}
+		else {
+			p = st.top();
+			st.pop();
+		}
+	}
+}
+
+/*
+ * 二叉树中序遍历
+ *
+ * 参数
+ *  visit ：该参数为访问树节点的函数指针，以实现对树节点的相关操作
+ */
+template<typename ElemType, typename Compare>
+inline void BTree<ElemType, Compare>::InOrder(void(*visit)(const ElemType& _Val))
+{
+	stack<BTNode<ElemType>*> st;
+	BTNode<ElemType>* p = m_root;
+
+	while (p || !st.empty()) {
+		if (p) {
+			st.push(p);
+			p = p->lchild;
+		}
+		else {
+			p = st.top();
+			visit(p->val);
+			st.pop();
+			p = p->rchild;
+		}
+	}
+}
+
+/*
+ * 二叉树后序遍历
+ *
+ * 参数
+ *  visit ：该参数为访问树节点的函数指针，以实现对树节点的相关操作
+ */
+template<typename ElemType, typename Compare>
+inline void BTree<ElemType, Compare>::PostOrder(void(*visit)(const ElemType& _Val))
+{
+	stack<BTNode<ElemType>*> st;
+	BTNode<ElemType>* r = NULL;
+	BTNode<ElemType>* p = m_root;
+
+	while (p || !st.empty()) {
+		if (p) {
+			st.push(p);
+			p = p->lchild;
+		}
+		else {
+			p = st.top();
+			if (p->rchild && p->rchild != r) {
+				p = p->rchild;
+			}
+			else {
+				visit(p->val);
+				st.pop();
+				r = p;
+				p = NULL;
+			}
+		}
+	}
+}
+
+/*
+ * 二叉树层次遍历
+ *
+ * 参数
+ *  visit ：该参数为访问树节点的函数指针，以实现对树节点的相关操作
+ */
+template<typename ElemType, typename Compare>
+inline void BTree<ElemType, Compare>::LevelOrder(void(*visit)(const ElemType& _Val))
+{
+	queue<BTNode<ElemType>*> qu;
+	BTNode<ElemType>* p = NULL;
+	qu.push(m_root);
+
+	while (!qu.empty()) {
+		p = qu.front();
+		visit(p->val);
+		qu.pop();
+
+		if (p->lchild) {
+			qu.push(p->lchild);
+		}
+		if (p->rchild) {
+			qu.push(p->rchild);
+		}
+	}
+}
+
+/*
+ * 获取二叉树中的最小值
+ *
+ * 返回值
+ *  ElemType ：如果树不为空，返回最小的元素值
+ *             如果树为空，则中断运行（获取最小值前，先调用 Empty() 方法判断树是否为空）
+ */
+template<typename ElemType, typename Compare>
+inline ElemType BTree<ElemType, Compare>::Minimum() const
+{
+	BTNode<ElemType>* p = minimum(m_root);
+	if (!p) {
+		abort();
+	}
+
+	return p->val;
+}
+
+/*
+ * 获取二叉树中的最大值
+ *
+ * 返回值
+ *  ElemType ：如果树不为空，返回最大的元素值
+ *             如果树为空，则中断运行（获取最大值前，先调用 Empty() 方法判断树是否为空）
+ */
+template<typename ElemType, typename Compare>
+inline ElemType BTree<ElemType, Compare>::Maximum() const
+{
+	BTNode<ElemType>* p = maximun(m_root);
+	if (!p) {
+		abort();
+	}
+
+	return p->val;
+}
+
+/*
+ * 获取树的节点个数
+ */
+template<typename ElemType, typename Compare>
+inline size_t BTree<ElemType, Compare>::Size() const
+{
+	return m_size;
+}
+
+/*
+ * 获取树的深度
+ */
+template<typename ElemType, typename Compare>
+size_t BTree<ElemType, Compare>::Depth() const
+{
+	if (!m_root) {
+		return 0;
+	}
+
+	size_t level = 0;
+	size_t n = 0;
+	BTNode<ElemType>* p = NULL;
+	queue<BTNode<ElemType>*> qu;
+	qu.push(m_root);
+
+	while (!qu.empty()) {
+		n = qu.size();
+		++level;
+
+		while (n--) {
+			p = qu.front();
+			qu.pop();
+			if (p->lchild) {
+				qu.push(p->lchild);
+			}
+			if (p->rchild) {
+				qu.push(p->rchild);
+			}
+		}
+	}
+}
+
+/*
+ * 树是否为空
+ *
+ * 返回值
+ *  bool ：树为空，返回 true
+ *         树不为空，返回 false
+ */
+template<typename ElemType, typename Compare>
+inline size_t BTree<ElemType, Compare>::Empty() const
+{
+	return m_size == 0 ? true : false;
+}
+
+/*
+ * 销毁一棵子树
+ *
+ * 参数
+ *  T ：待销毁的子树根节点
+ */
+template<typename ElemType, typename Compare>
+void BTree<ElemType, Compare>::destroy(BTNode<ElemType>* T)
+{
+	if (T) {
+		destroy(T->lchild);
+		destroy(T->rchild);
+		delete T;
+		T = NULL;
+	}
+}
+
+/*
+ * 查找元素所对应的节点指针
+ *
+ * 参数：
+ *  _Val ：待查找的元素值
+ *
+ * 返回值
+ *  BTNode<ElemType>* ：如果存在该元素值，则返回对应的节点指针
+ *                      如果不存在该元素值，则返回 NULL
+ */
+template<typename ElemType, typename Compare>
+BTNode<ElemType>* BTree<ElemType, Compare>::search(const ElemType& _Val)
+{
+	BTNode<ElemType>* p = m_root;
+	size_t n = 0;
+
+	while (p) {
+		n = m_cmp(p->val, _Val);
+		if (n == 0) {
+			break;
+		}
+		else if (n < 0) {
+			p = p->lchild;
+		}
+		else {
+			p = p->rchild;
+		}
+	}
+
+	return p;
+}
+
+/*
+ * 获取子树 T 中的最左节点(最小值节点)
+ */
+template<typename ElemType, typename Compare>
+BTNode<ElemType>* BTree<ElemType, Compare>::minimum(BTNode<ElemType>* T)
+{
+	BTNode<ElemType>* p = T;
+	if (p) {
+		while (p->lchild) {
+			p = p->lchild;
+		}
+	}
+
+	return p;
+}
+
+/*
+ * 获取子树 T 的右节点(最大值节点)
+ */
+template<typename ElemType, typename Compare>
+BTNode<ElemType>* BTree<ElemType, Compare>::maximun(BTNode<ElemType>* T)
+{
+	BTNode<ElemType>* p = T;
+	if (p) {
+		while (p->rchild) {
+			p = p->rchild;
+		}
+	}
+
+	return p;
+}
