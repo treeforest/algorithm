95429e94b072640b00d17c8329393b9976de6a82
勇 张 <1451670604@qq.com>
2020-05-15 21:23:51 +0800

    fix: CountSort error

--- sort/CountSort/main.cpp
+++ sort/CountSort/main.cpp
@@ -10,31 +10,39 @@ using namespace std;
  *  len：数组 A 的长度（也即 B 的长度）
  *  k：要求 A 中的每一个数组都是 0~k 的数
  */
-void CountSort(int A[], int B[], int len, int k) {
+void CountSort(int* A, int* B, int len, int k) {
 	int i, j;
 	int* C = new int[k + 1]();
 	
 	// make C[i] contains the number of elements equal to i.
 	for (i = 0; i < len; i++) {
-		C[A[i]] = C[A[i]] + 1;
+		C[A[i]]++;
 	}
 
 	// make C[i] contains the number of elements less than or equal to i.
 	for (i = 1; i <= k; i++) {
 		C[i] = C[i] + C[i - 1];
 	}
-
+	
 	for (j = len - 1; j >= 0; j--) {
-		B[C[A[j]]] = A[j];
+		B[C[A[j]]-1] = A[j];
 		C[A[j]] = C[A[j]] - 1;
 	}
 }
 
 int main() {
-
 	int A[] = { 2,5,3,0,2,3,0,3 };
 	int B[8] = {};
-	CountSort(A, B, sizeof(A) / sizeof(int), 5);
+
+	CountSort(A, B, 8, 5);
+
+	for (int i = 0; i < 8; i++) {
+		cout << A[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < 8; i++) {
+		cout << B[i] << " ";
+	}
 
 	return 0;
 }
