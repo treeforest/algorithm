54f221805fc20e9dba71797fd33b6120ad13972c
treeforest <1451670604@qq.com>
2021-01-28 23:41:48 +0800

    feat: 1001

--- PAT/1001/main.go
+++ PAT/1001/main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var a, b, sum int
+	fmt.Scanf("%d %d", &a, &b)
+	sum = a + b
+	s := fmt.Sprintf("%d", sum)
+	if sum < 0 {
+		s = s[1:]
+	}
+
+	var s2 string = ""
+	var i int = 0
+	var j int = 0
+
+	for i = len(s) - 1; i >= 0; i-- {
+		if j%3 == 0 && j > 0{
+			s2 = "," + s2
+		}
+		s2 = string(s[i]) + s2
+		j++
+	}
+
+	if sum < 0 {
+		s2 = "-" + s2
+	}
+
+	fmt.Print(s2)
 }
