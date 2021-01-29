bc2f9f3b2684ae961ce847c816643e0c89590c1c
treeforest <1451670604@qq.com>
2021-01-29 16:10:27 +0800

    feat: PAT 1002

--- PAT/1002/main.go
+++ PAT/1002/main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("1002")
+	s := make([]float32, 1001)
+	var k, i int
+	var num float32
+	fmt.Scanf("%d", &k)
+	for j := 0; j < k; j++ {
+		fmt.Scanf("%d%f", &i, &num)
+		s[i] += num
+	}
+	fmt.Scanf("%d", &k)
+	for j := 0; j < k; j++ {
+		fmt.Scanf("%d%f", &i, &num)
+		s[i] += num
+	}
+	var cnt int = 0
+	for j := 1000; j >= 0; j-- {
+		if s[j] != 0.0 {
+			cnt++
+		}
+	}
+	fmt.Print(cnt)
+	for j := 1000; j >= 0; j-- {
+		if s[j] != 0.0 {
+			fmt.Printf(" %d %.1f", j, s[j])
+		}
+	}
 }
