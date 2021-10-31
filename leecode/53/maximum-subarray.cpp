c45666a241537909d87fc53fb50d586aa74f960d
treeforest <1451670604@qq.com>
2021-10-31 23:53:32 +0800

    update 53

---
+++ leecode/53/maximum-subarray.cpp
@@ -0,0 +1,27 @@
+func maxSubArray(nums[]int) int{
+    if len(nums) == 0 {
+        return 0
+    }
+    if len(nums) == 1{
+        return nums[0]
+    }
+    dp: = make([]int, len(nums))
+    dp[0] = nums[0]
+    res : = nums[0]
+    for i : = 1; i < len(nums); i++ {
+        if dp[i - 1] > 0 {
+            dp[i] = nums[i] + dp[i - 1]
+        }
+     else {
+      dp[i] = nums[i]
+    }
+        res = max(dp[i], res)
+    }
+        return res
+    }
+func max(a, b int) int{
+    if a > b {
+        return a
+    }
+    return b
+}
