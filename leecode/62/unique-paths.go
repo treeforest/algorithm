e64947e0ebdadae6f56663cd37548b13334cf5c7
treeforest <1451670604@qq.com>
2021-11-01 09:25:33 +0800

    update 62

---
+++ leecode/62/unique-paths.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	for i := 0; i < m; i++ { // 行
+		for j := 0; j < n; j++ { // 列
+			if i == 0 || j == 0 {
+				dp[i][j] = 1
+				continue
+			}
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
