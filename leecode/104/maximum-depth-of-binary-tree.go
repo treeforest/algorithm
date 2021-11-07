ca3338bff482e87f1b3ec92b83029d8b51a78def
treeforest <1451670604@qq.com>
2021-11-07 16:36:27 +0800

    update 104

---
+++ leecode/104/maximum-depth-of-binary-tree.go
@@ -0,0 +1,31 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	queue := []*TreeNode{root}
+	high := 0
+	for len(queue) != 0 {
+		n := len(queue)
+		for i := 0; i < n; i++ {
+			if queue[i].Left != nil {
+				queue = append(queue, queue[i].Left)
+			}
+			if queue[i].Right != nil {
+				queue = append(queue, queue[i].Right)
+			}
+		}
+		high++
+		queue = queue[n:]
+	}
+	return high
+}
