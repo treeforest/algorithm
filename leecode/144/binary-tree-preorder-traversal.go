ee9b063d99a48e922506543975b8d34907555abe
treeforest <1451670604@qq.com>
2021-11-09 11:06:18 +0800

    update 144

---
+++ leecode/144/binary-tree-preorder-traversal.go
@@ -0,0 +1,23 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+	res := []int{}
+	preorder(root, &res)
+	return res
+}
+
+func preorder(node *TreeNode, res *[]int) {
+	if node != nil {
+		*res = append(*res, node.Val)
+		preorder(node.Left, res)
+		preorder(node.Right, res)
+	}
+}
