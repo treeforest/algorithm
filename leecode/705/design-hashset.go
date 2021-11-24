db525f5dc416c159ae8ef95c1313e7de4aa022f7
treeforest <1451670604@qq.com>
2021-11-24 22:15:59 +0800

    update 705

--- leecode/705/design-hashset.go
+++ leecode/705/design-hashset.go
@@ -1,23 +1,54 @@
 package leetcode
 
 type MyHashSet struct {
-	keys []bool
+	set     [][]int
+	hashKey int
 }
 
 func Constructor() MyHashSet {
-	return MyHashSet{keys: make([]bool, 1000001, 1000001)}
+	hashKey := 111
+	set := make([][]int, hashKey)
+	for i, _ := range set {
+		set[i] = make([]int, 0)
+	}
+	return MyHashSet{
+		set:     set,
+		hashKey: hashKey,
+	}
+}
+
+func (this *MyHashSet) hash(key int) int {
+	return key % this.hashKey
 }
 
 func (this *MyHashSet) Add(key int) {
-	this.keys[key] = true
+	i := this.hash(key)
+	for _, v := range this.set[i] {
+		if v == key {
+			return
+		}
+	}
+	this.set[i] = append(this.set[i], key)
 }
 
 func (this *MyHashSet) Remove(key int) {
-	this.keys[key] = false
+	i := this.hash(key)
+	for j, v := range this.set[i] {
+		if v == key {
+			this.set[i] = append(this.set[i][:j], this.set[i][j+1:]...)
+			return
+		}
+	}
 }
 
 func (this *MyHashSet) Contains(key int) bool {
-	return this.keys[key]
+	i := this.hash(key)
+	for _, v := range this.set[i] {
+		if v == key {
+			return true
+		}
+	}
+	return false
 }
 
 /**
